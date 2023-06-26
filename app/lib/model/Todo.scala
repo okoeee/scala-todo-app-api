818a781b5cf105b427a3adf79b201f0b2c48e1f0
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-06-26 15:44:49 +0900

    TODO Statusのnameを変更

--- app/lib/model/Todo.scala
+++ app/lib/model/Todo.scala
@@ -22,7 +22,7 @@ object Todo {
 
   sealed abstract class Status(val code: Short, val name: String) extends EnumStatus
   object Status extends EnumStatus.Of[Status] {
-    case object IS_STARTED extends Status(code = 0, name = "着手中")
+    case object IS_STARTED extends Status(code = 0, name = "未着手")
     case object IS_PROGRESSIVE extends Status(code = 1, name = "進行中")
     case object IS_COMPLETED extends Status(code = 2, name = "完了")
   }
