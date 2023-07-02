48b470233f7fc88fe4db71d507ec9f6b0306f09d
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-02 17:23:23 +0900

    Todo新規作成のAPIを追加

--- app/controllers/api/TodoController.scala
+++ app/controllers/api/TodoController.scala
@@ -1,23 +1,54 @@
 package controllers.api
 
+import json.reads.JsValueCreateTodo
 import json.writes.JsValueTodoListItem
-import play.api.libs.json.Json
+import lib.model.{Category, Todo}
+import lib.persistence.onMySQL.TodoRepository
+import play.api.libs.json.{JsError, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import service.TodoService
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class TodoController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController
-    with play.api.i18n.I18nSupport {
+  with play.api.i18n.I18nSupport {
 
   def index(): Action[AnyContent] = Action.async { implicit req =>
     TodoService.getSeqViewValueTodo.map { seqViewValueTodo =>
-      val seqJsValue = seqViewValueTodo.map { viewValueTodo => JsValueTodoListItem(viewValueTodo) }
+      val seqJsValue = seqViewValueTodo.map { viewValueTodo =>
+        JsValueTodoListItem(viewValueTodo)
+      }
       Ok(Json.toJson(seqJsValue))
     }
+  }
+
+  def createAction(): Action[JsValue] = Action(parse.json).async {
+    implicit req =>
+      req.body
+        .validate[JsValueCreateTodo]
+        .fold(
+          errors => {
+            val errMsg = JsError.toJson(errors).toString
+            Future.successful(
+              BadRequest(Json.obj("status" -> "error", "message" -> errMsg)))
+          },
+          todoData => {
+            val todoWithNoId: Todo#WithNoId = Todo(
+              categoryId = Category.Id(todoData.categoryId),
+              title = todoData.title,
+              body = todoData.body,
+              state = Todo.Status.IS_STARTED
+            )
+
+            TodoRepository.add(todoWithNoId).map { _ =>
+              Ok(Json.obj("status" -> "ok", "message" -> "Todoを作成しました"))
+            }
+          }
+        )
   }
 
 }
---
+++ app/json/reads/JsValueCreateTodo.scala
@@ -0,0 +1,13 @@
+package json.reads
+
+import play.api.libs.json.{Json, Reads}
+
+case class JsValueCreateTodo (
+  title: String,
+  body: String,
+  categoryId: Long
+)
+
+object JsValueCreateTodo {
+  implicit val reads: Reads[JsValueCreateTodo] = Json.reads[JsValueCreateTodo]
+}
