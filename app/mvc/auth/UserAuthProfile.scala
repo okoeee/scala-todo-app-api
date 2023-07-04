0344be4209287a51bdd89108c363f15f860fe504
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-04 12:26:01 +0900

    userAuthProfile定義

---
+++ app/mvc/auth/UserAuthProfile.scala
@@ -0,0 +1,33 @@
+package mvc.auth
+
+import ixias.play.api.auth.container.Container
+import ixias.play.api.auth.mvc.AuthProfile
+import ixias.play.api.auth.token.Token
+import ixias.play.api.auth.token.TokenViaSession
+import lib.model.User
+import lib.persistence.onMySQL.UserRepository
+import play.api.Environment
+import play.api.mvc.RequestHeader
+
+import java.time.Duration
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+case class UserAuthProfile @Inject()(
+  val container: AuthTokenContainer
+)(implicit ec: ExecutionContext)
+  extends AuthProfile[User.Id, User, Unit] {
+
+  val env: Environment = Environment.simple()
+  val tokenAccessor: Token = TokenViaSession("user")
+  val datastore: Container[Id] = container
+  val executionContext: ExecutionContext = ec
+
+  override def resolve(id: Id)(
+    implicit rh: RequestHeader): Future[Option[AuthEntity]] =
+    UserRepository.get(id)
+
+  override def sessionTimeout(
+    implicit request: RequestHeader): Option[Duration] =
+    Some(Duration.ofHours(24L))
+}
