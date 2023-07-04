20dd81dda629b1a57ec240f8b7b602ec9d491a3a
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-04 11:17:10 +0900

    User modelを修正

--- app/lib/persistence/User.scala
+++ app/lib/persistence/User.scala
@@ -1,8 +1,3 @@
-/**
-  * This is a sample of Todo Application.
-  * 
-  */
-
 package lib.persistence
 
 import scala.concurrent.Future
@@ -10,8 +5,6 @@ import ixias.persistence.SlickRepository
 import lib.model.User
 import slick.jdbc.JdbcProfile
 
-// UserRepository: UserTableへのクエリ発行を行うRepository層の定義
-//~~~~~~~~~~~~~~~~~~~~~~
 case class UserRepository[P <: JdbcProfile]()(implicit val driver: P)
   extends SlickRepository[User.Id, User, P]
   with db.SlickResourceProvider[P] {
@@ -22,28 +15,25 @@ case class UserRepository[P <: JdbcProfile]()(implicit val driver: P)
     * Get User Data
     */
   def get(id: Id): Future[Option[EntityEmbeddedId]] =
-    RunDBAction(UserTable, "slave") { _
-      .filter(_.id === id)
-      .result.headOption
-  }
+    RunDBAction(UserTable, "slave") { _.filter(_.id === id).result.headOption }
 
   /**
     * Add User Data
-   */
+    */
   def add(entity: EntityWithNoId): Future[Id] =
     RunDBAction(UserTable) { slick =>
       slick returning slick.map(_.id) += entity.v
     }
 
   /**
-   * Update User Data
-   */
+    * Update User Data
+    */
   def update(entity: EntityEmbeddedId): Future[Option[EntityEmbeddedId]] =
     RunDBAction(UserTable) { slick =>
       val row = slick.filter(_.id === entity.id)
       for {
         old <- row.result.headOption
-        _   <- old match {
+        _ <- old match {
           case None    => DBIO.successful(0)
           case Some(_) => row.update(entity.v)
         }
@@ -51,14 +41,14 @@ case class UserRepository[P <: JdbcProfile]()(implicit val driver: P)
     }
 
   /**
-   * Delete User Data
-   */
+    * Delete User Data
+    */
   def remove(id: Id): Future[Option[EntityEmbeddedId]] =
     RunDBAction(UserTable) { slick =>
       val row = slick.filter(_.id === id)
       for {
         old <- row.result.headOption
-        _   <- old match {
+        _ <- old match {
           case None    => DBIO.successful(0)
           case Some(_) => row.delete
         }
