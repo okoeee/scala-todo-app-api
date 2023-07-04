c3e3351c9f9ac1afb7dc230753f8a80d350ef46b
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-04 12:53:58 +0900

    loginsuccededを追加

--- app/mvc/auth/UserAuthProfile.scala
+++ app/mvc/auth/UserAuthProfile.scala
@@ -3,11 +3,12 @@ package mvc.auth
 import ixias.play.api.auth.container.Container
 import ixias.play.api.auth.mvc.AuthProfile
 import ixias.play.api.auth.token.Token
+import ixias.play.api.auth.token.Token.AuthenticityToken
 import ixias.play.api.auth.token.TokenViaSession
 import lib.model.User
 import lib.persistence.onMySQL.UserRepository
 import play.api.Environment
-import play.api.mvc.RequestHeader
+import play.api.mvc.{RequestHeader, Result}
 
 import java.time.Duration
 import javax.inject.Inject
@@ -30,4 +31,11 @@ case class UserAuthProfile @Inject()(
   override def sessionTimeout(
     implicit request: RequestHeader): Option[Duration] =
     Some(Duration.ofHours(24L))
+
+//  override def loginSucceeded(id: Id, block: AuthenticityToken => Result)(
+//    implicit rh: RequestHeader): Future[Result] =
+//    for {
+//      token <- datastore.open(id, sessionTimeout)
+//    } yield tokenAccessor.put(token)(block(token))
+
 }
