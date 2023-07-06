f55c93fe8dc54143c0097409607e9b6701f67736
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-06 10:50:35 +0900

    todo(json)の型を修正

--- app/model/Todo.scala
+++ app/model/Todo.scala
@@ -1,11 +1,11 @@
 package model
 
-import lib.model.Category
+import lib.model.{Category, Todo}
 import lib.model.Todo.Status
 import lib.model.Category.CategoryColor
 
 case class ViewValueTodo(
-  id: Long,
+  id: Todo.Id,
   title: String,
   body: String,
   status: Status,
